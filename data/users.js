c5f18975ef02feb1a88e282ba2fad552b18a3354
kaikune <alexkai03@live.com>
2023-12-02 23:58:23 -0500

    Implemented deleteUser

--- data/users.js
+++ data/users.js
@@ -1,20 +1,14 @@
 // This data file should export all functions using the ES6 standard as shown in the lecture code
 
 import * as helpers from '../helpers.js';
-import { users } from '../config/mongoCollections.js';
-//import eventsFunctions from './events.js';
+import { users, games, groups } from '../config/mongoCollections.js';
 import { ObjectId } from 'mongodb';
 import bcrypt from 'bcrypt';
 
-
-
-
-
-const createUser = async (username,emailAddress, password) => {
+const createUser = async (username, emailAddress, password) => {
     // Input Validation
-  
+
     helpers.validateUser(username, emailAddress, password);
-    
 
     username = username.trim();
     password = password.trim();
@@ -23,13 +17,14 @@ const createUser = async (username,emailAddress, password) => {
     // Search for users
     const userCollection = await users();
     let userList = await userCollection.find({}).toArray();
+
     // Check for existing email
-    if (!userList){
+    if (!userList) {
         throw 'Could not get all users';
     }
-    userList = userList.filter(obj => obj.emailAddress === emailAddress);
-    if(userList.length > 0){
-        throw "Email address already taken"
+    userList = userList.filter((obj) => obj.emailAddress === emailAddress);
+    if (userList.length > 0) {
+        throw 'Email address already taken';
     }
     const saltRounds = 16;
     const hashPass = await bcrypt.hash(password, saltRounds);
@@ -39,43 +34,47 @@ const createUser = async (username,emailAddress, password) => {
         _id: new ObjectId(),
         username,
         emailAddress,
-        description: "",
+        description: '',
         profilePicture: null,
         password: hashPass,
         friends: [],
         games: [],
-        groups: []
+        groups: [],
     };
 
     // Update the user
     const insertInfo = await userCollection.insertOne(newUser);
-    if (!insertInfo.acknowledged || !insertInfo.insertedId){
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) {
         throw 'Could not add user';
     }
-    return {insertedUser: true};
+
+    return { insertedUser: true };
 };
-const updateUserBio = async (userId, username, profilePicture, description) =>{
+
+const updateUserBio = async (userId, username, profilePicture, description) => {
     if (!userId) throw 'User Id not given';
     if (typeof userId !== 'string') throw 'User Id is not a string';
     userId = userId.trim();
     if (!ObjectId.isValid(userId)) throw 'User Id is not valid';
-    helpers.validateUserBio(username,profilePicture,description);
+    helpers.validateUserBio(username, profilePicture, description);
     username = username.trim();
     profilePicture = profilePicture.trim();
     description = description.trim();
     const userCollection = await users();
     await userCollection.updateOne(
-     {  _id: new ObjectId(userId) },
-     { $set: {
-      username : username,
-      description : description,
-      profilePicture : profilePicture 
-    } }
+        { _id: new ObjectId(userId) },
+        {
+            $set: {
+                username: username,
+                description: description,
+                profilePicture: profilePicture,
+            },
+        }
     );
     const user = await getUser(userId);
     return user;
-}
-const getAllUsers = async () =>{
+};
+const getAllUsers = async () => {
     const userCollection = await users();
     let userList = await userCollection.find({}).toArray();
     if (!userList) throw 'Could not get all events';
@@ -84,7 +83,7 @@ const getAllUsers = async () =>{
         return element;
     });
     return userList;
-}
+};
 
 const getUser = async (userId) => {
     // Input Validation
@@ -96,38 +95,54 @@ const getUser = async (userId) => {
     // Get all users
     const userCollection = await users();
     let userList = await userCollection.find({}).toArray();
-    const user = userList.find(user => user._id.toString() === userId);
-    if(!user){
-        throw "Could not find user"
+    const user = userList.find((user) => user._id.toString() === userId);
+    if (!user) {
+        throw 'Could not find user';
     }
     user._id = user._id.toString();
     return user;
 };
 
-const deleteUser = async (userId) => {//Not implemented waiting until games and groups are finished since we will want to remove from them when deleting a user
+const deleteUser = async (userId) => {
     // Input Validation
     if (!userId) throw 'User Id not given';
     if (typeof userId !== 'string') throw 'User Id is not a string';
     userId = userId.trim();
     if (!ObjectId.isValid(userId)) throw 'User Id is not valid';
 
-    const eventCollection = await events();
+    const gameCollection = await games();
+    const groupCollection = await groups();
 
-    const remove = await eventCollection.findOneAndUpdate(
-        { 'users._id': new ObjectId(userId) },
+    // Remove user from all games
+    const gameRemove = await gameCollection.updateMany(
+        { 'players._id': new ObjectId(userId) },
         {
             $pull: {
-                users: {
+                players: {
                     _id: new ObjectId(userId), // remove user
                 },
             },
-            $inc: { totalNumberOfUsers: -1 },
+            $inc: { totalNumberOfPlayers: -1 },
         },
         { returnDocument: 'after' }
     );
 
-    if (!remove) throw 'Could not delete user';
-    return remove;
+    // Remove user from all groups
+    const groupRemove = await groupCollection.updateMany(
+        { 'players.id': new ObjectId(userId) },
+        {
+            $pull: {
+                players: {
+                    _id: new ObjectId(userId), // remove user
+                },
+            },
+            $inc: { totalNumberOfPlayers: -1 },
+        },
+        { returnDocument: 'after' }
+    );
+
+    if (!gameRemove || !groupRemove) throw 'Could not delete user';
+    return { gameRemove, groupRemove };
 };
 
-export default { createUser, getAllUsers, getUser, deleteUser,updateUserBio };
+export default { createUser, getAllUsers, getUser, deleteUser, updateUserBio };
