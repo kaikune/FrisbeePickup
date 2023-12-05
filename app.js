562e81744d6e526fc1bef8f01ca9a21c27c8675d
zacky24 <124016155+zacky24@users.noreply.github.com>
2023-12-05 02:35:14 -0500

    Serving static assets

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import exphbs from 'express-handlebars';
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
 
 import configRoutesFunction from './routes/index.js';
 
 const app = express();
 
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const staticDir = express.static(__dirname + '/public');
+app.use('/public', staticDir);
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
