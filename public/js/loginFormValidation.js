072cff516ae6d85fd92bdb414006db81143a6e60
lMoosel <ccanata@stevens.edu>
2023-12-10 14:12:41 -0500

    Finally some client side JS

--- public/js/loginFormValidation.js
+++ public/js/loginFormValidation.js
@@ -3,15 +3,15 @@ let loginForm = document.getElementById("login-form")
 if(loginForm){
     let errorLabel = document.getElementById("error-label")
     errorLabel.hidden = true;
-    //LISTEN FOR FORM SUBMISSION
-    event.preventDefault();
-    try{
-    //Email Validation
-    //Password Validation
-    }
-    catch(err){
-        errorLabel.innerHTML = err;
-        errorLabel.hidden = false;
-    }
-
+    loginForm.addEventListener('submit', (event) => {
+        try{
+        //Email Validation
+        //Password Validation
+        }
+        catch(err){
+            event.preventDefault();
+            errorLabel.innerHTML = err;
+            errorLabel.hidden = false;
+        }
+    })
 }
