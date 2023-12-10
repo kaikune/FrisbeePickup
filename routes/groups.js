ea590a4c2ecea060ad3049a5a7136d123068e5ef
kaikune <alexkai03@live.com>
2023-12-10 11:53:04 -0500

    Added comment posting to group

--- routes/groups.js
+++ routes/groups.js
@@ -8,6 +8,7 @@ router
     .route("/")
     .get(async (req, res) => {
         let allGroupsData = await groupsData.getAll();
+        
         return res.json(allGroupsData);
     });
 
@@ -17,6 +18,7 @@ router
         try {
             let groupId = req.params.groupId;
             let groupRes = await groupsData.get(groupId);
+
             return res.json(groupRes);
         } catch (e) {
             res.status(400);
@@ -24,4 +26,18 @@ router
         }
     });
 
+router
+    .route('/:groupId/comments')
+    .post(async (req, res) => {
+        try {
+            let groupId = req.params.groupId;
+            let comment = req.body.comment;
+            let userId = req.body.userId;
+
+            let groupRes = await groupsData.addComment(groupId, userId, comment)
+            return res.json(groupRes);
+        } catch (e) {
+            res.status(500).json({error: e})
+        }
+    })
 export default router;
