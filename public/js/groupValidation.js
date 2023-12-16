fe43e7abf8361c9d3f267c78d1982bca38f8cf9d
Luke Roumy <Luke5r@icloud.com>
2023-12-16 12:43:51 -0500

    Fixed create group

--- public/js/groupValidation.js
+++ public/js/groupValidation.js
@@ -1,17 +1,19 @@
 let createGroupForm = document.getElementById("create-group-form");
 
 if(createGroupForm){
-    let errorLabel = document.getElementById("error-label")
-    let statusLabel = document.getElementById("status-label")
+    let errorLabel = document.querySelector('.error-label');
+    let statusLabel = document.querySelector('.status-label');
     errorLabel.hidden = true;
 
-    loginForm.addEventListener('submit', (event) => {
+    createGroupForm.addEventListener('submit', (event) => {
+        event.preventDefault();
         try{
             let groupName = document.getElementById("group-name")
             let groupDescription = document.getElementById("description")
-            //validateGroup(groupName, groupDescription, SOMETHING SO THAT IT VALIDATES)
+            validateGroup(groupName.value, groupDescription.value)
             statusLabel.innerHTML = "Group Created"
             statusLabel.hidden = false;
+            createGroupForm.submit()
         }
         catch(err){
             event.preventDefault();
@@ -19,4 +21,19 @@ if(createGroupForm){
             errorLabel.hidden = false;
         }
     })
+}
+
+function validateGroup(groupName, groupDescription) {
+    // Input Validation
+    if (groupName == null || groupDescription == null) throw 'All fields need to have valid values';
+
+    if (typeof groupName !== 'string' || typeof groupDescription !== 'string') throw 'One or more string fields not given as string';
+
+    groupName = groupName.trim();
+    groupDescription = groupDescription.trim();
+
+    if (groupName.length === 0 || groupDescription.length === 0) throw 'One or more string fields empty';
+
+    if (groupName.length < 5) throw 'group name less than 5 chars';
+    if (groupDescription.length < 25) throw 'group description less than 25 chars';
 }
