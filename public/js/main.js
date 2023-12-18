dc12667b0aa5cb17f9c3960164bb6eed09f5963a
zacky24 <124016155+zacky24@users.noreply.github.com>
2023-12-17 20:39:16 -0500

    replaced email validation functions

--- public/js/main.js
+++ public/js/main.js
@@ -67,8 +67,54 @@ const states = [
     'WV',
     'WY',
 ];
-function isValidEmail (contactEmail) {
-    return /^([a-z0-9]+|([a-z0-9]+[.-_]*[a-z0-9]))+@[a-z0-9-]+\.[a-z]{2,}$/.test(contactEmail);
+let charIsLowercase = function (c) {
+    return c >= "a" && c <= "z";
+}
+let charIsNumber = function (c) {
+    return c >= "0" && c <= "9";
+}
+let isValidEmail = function (email) {
+    // Based on https://help.xmatters.com/ondemand/trial/valid_email_format.htm
+    if (!email.includes("@")) return false;
+    let s = email.split("@");
+    if (s.length != 2) return false;
+    let [prefix, domain] = s;
+    if (!prefix.length || !domain.length) return false;
+
+    for (let i = 0; i < prefix.length; i++) {
+        if (
+            charIsLowercase(prefix[i])
+            ||
+            charIsNumber(prefix[i])
+        ) {
+            continue;
+        } else if ("_.-".includes(prefix[i])) {
+            if (!i) return false;
+            if (i == prefix.length - 1) return false;
+            if ("_.-".includes(prefix[i - 1])) return false;
+            continue;
+        } else {
+            return false;
+        }
+    }
+
+    let idx = -1;
+    for (let i = domain.length - 1; i >= 0; i--) {
+        if (domain[i] == ".") {
+            idx = i;
+            break;
+        }
+    }
+    if (idx == -1 || idx == 0 || idx == domain.length - 1) return false;
+    let tld = domain.substring(idx + 1);
+    let site = domain.substring(0, idx - 1);
+    for (let i = 0; i < site.length; i++) {
+        if (!(
+            charIsLowercase(site[i]) || charIsNumber(site[i]) || site[i] == "-"
+        )) { return false }
+    }
+    if (tld.length < 2) return false;
+    return true;
 }
 function validatePassword (password) {
     if (password.length < 8) {
